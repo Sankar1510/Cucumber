d30b1e882ab0636da25dd46f5721fea2657a82d1
Sankar1510 <bsankar1510@gmail.com>
2022-12-15 09:47:06 +0000

    data success

---
+++ src/main/java/com/mobil/App.java
@@ -0,0 +1,13 @@
+package com.mobil;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
